3ee4f04138501d891c570992e66f2d1fbae70b02
plutasnyy <kamil.plucinski97@gmail.com>
2020-05-02 15:02:23 +0200

    iterated ls

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from heapq import nsmallest
 from itertools import product
 
 import seaborn as sns
+from IPython.core.display import display
 from tqdm import tqdm
 
 from api.instance import Instance
+from strategies.iterated_local_search.iterated_local_search import IteratedLocalSearch
 from strategies.local_search.local_search import LocalSearch
 from strategies.ls_cache.local_search_with_cache import LocalSearchWitchCache
 from strategies.multiple_local_search.multiple_local_search import MultipleStartLocalSearch
@@ -13,13 +15,14 @@ import pandas as pd
 import numpy as np
 
 sns.set()
-df = pd.DataFrame(columns=['version', 'neigbourhood', 'cost', 'time'])
+df = pd.DataFrame(columns=['version', 'instance', 'cost', 'time'])
 for instance_name in ['kroA100', 'kroB100']:
     instance = Instance(name=instance_name)
-    solve_strategy: MultipleStartLocalSearch = MultipleStartLocalSearch(
+    solve_strategy: IteratedLocalSearch = IteratedLocalSearch(
         instance=instance,
+        perturbation=10
     )
-    solve_strategy.run(run_times=2)
+    solve_strategy.run(run_times=1)
     # raise KeyError
     costs = list(map(lambda x: x[1], solve_strategy.solutions))
     times = list(map(lambda x: x[2], solve_strategy.solutions))
@@ -27,7 +30,8 @@ for instance_name in ['kroA100', 'kroB100']:
     print(instance_name, min(costs), np.mean(times))
 
     for s, cost, time in solve_strategy.solutions:
-        df = df.append(pd.DataFrame([[cost, time]], columns=['cost', 'time']))
+        df = df.append(
+            pd.DataFrame([['iterated', instance_name, cost, time]], columns=['version', 'instance', 'cost', 'time']))
 
     # draw_solution(
     #     instance=instance,
@@ -35,3 +39,4 @@ for instance_name in ['kroA100', 'kroB100']:
     #     title=f'Local search {version}, {instance.name}, distance: {solve_strategy.solution_cost}, {neighbourhood}',
     #     save_file_name=f'{instance.name}_{min(costs)}_{version}_{neighbourhood}.png'
     # )
+display(df)
